df8d7a780d8903f8afe99e7280775bbce9af076a
Hanming Zhu <hanming@huawen.com>
2021-07-28 12:26:28 +0800

    Fix unused import

--- src/components/ChatRenderer.jsx
+++ src/components/ChatRenderer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { CSSTransition } from "react-transition-group";
 import { animateScroll as scroll } from "react-scroll";
 
